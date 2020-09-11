74365fb1f1b9bc97dbb1277218bd3c9b896f6962
aabi0044 <muhammad.abdullah.10.1995@gmail.com>
2020-09-11 23:05:23 +0500

    implemented delete to do

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -31,7 +31,10 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(item => item.id !== id);
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res=> this.todos = this.todos.filter(todo=>todo.id!==id))
+         .catch(err => console.log(err));
+        this.todos = this.todos.filter(item => item.id !== id);
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
